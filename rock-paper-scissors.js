8c228adddfbc8c2bd1668e6e0fd62cd805337a98
Tim Narimanov <sovremenniy@gmail.com>
2014-08-08 14:33:11 +0300

    Теперь на русском
    Я перевел прототип игры на русский язык! На здоровье!

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,42 +1,42 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
-var computerChoice = Math.random();
-if (computerChoice <0.34){
-        computerChoice = "rock";
-}else if(computerChoice <=0.67){
-        computerChoice = "paper";
-}else{
-        computerChoice = "scissors";
+var userChoice = prompt("Ты выбираешь камень, ножницы или же бумагу?");
+var computerChoice = Math.floor(Math.random() * 4);
+if (computerChoice === 1){
+        computerChoice = "камень";
+} else if(computerChoice === 2){
+        computerChoice = "бумага";
+} else {
+        computerChoice = "ножницы";
 }
  
 var compare = function(choice1, choice2) {
         if (choice1 === choice2) {
-                return("The result is a tie!");
+                return("Ничья, хоуми! Давай ещё раз!");
         }
        
-        if (choice1 === "rock") {
-                if (choice2 === "scissors") {
-                        return("Rock wins.");
+        if (choice1 === "камень") {
+                if (choice2 === "ножницы") {
+                        return("Ты выиграл! Если что, соперник выбрал ножницы.");
                 } else {
-                        return("Paper wins.");
+                        return("Боюсь, что ты програл! Просто случилось так, что противник решил накрыть тебя бумагой.");
                 }
         }
        
-        if (choice1 === "paper") {
-                if (choice2 === "rock") {
-                        return("paper wins.");
+        if (choice1 === "бумага") {
+                if (choice2 === "камень") {
+                        return("Молодец! Ты только что накрыл пацана бумагой.");
                 } else {
-                        return ("Scissors wins");
+                        return ("Соперник разрезал твою ничтожную бумажку...");
                 }
         }
        
-        if (choice1 === "scissors") {
-                if (choice2 === "paper") {
-                        return("Scissors wins.");
+        if (choice1 === "ножницы") {
+                if (choice2 === "бумага") {
+                        return("Вы разрезали врага на мелкие кусочки.");
                 } else {
-                        return ("rock wins");
+                        return ("Камень соперника даже лучше ваших ножниц!");
                 }
         } else {
-                console.log("I know you did something wrong. It may be just because you tried to troll me, so fuck you, user.")
+                console.log("Думал меня потриллить? Тролль нашелся у нас тут!")
         }
        
 };
